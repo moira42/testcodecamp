7055caecbb0195e0ae513597adf1645e91804bb1
Moira Amadi-Emina <90391409+moira42@users.noreply.github.com>
2021-09-21 14:01:31 +0100

    Add files via upload

---
+++ JavaDataTypes.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class JavaDataTypes {
+    public static void main(String[] args) {
+
+
+
+
+
+
+
+
+
+        byte age =-127;
+
+        short money = 32_76;
+
+        int massiveMoney = 2147483647;
+
+        long soMuchMoney = -2147483649l;
+
+        float area = 1234567.90f;
+
+        double biggerArea = 123456.67;
+
+        System.out.println(age);
+        System.out.println(money);
+        System.out.println(massiveMoney);
+        System.out.println(soMuchMoney);
+        System.out.println(area);
+        System.out.println(biggerArea);
+    }
+}
+
+
+
