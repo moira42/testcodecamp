68106f743b0da89ce7ce993b3f3b62bbd7276ce4
Moira Amadi-Emina <90391409+moira42@users.noreply.github.com>
2021-09-23 13:58:26 +0100

    Add files via upload
    StringOpsTask18

---
+++ StringOpsTask18.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class StringOpsTask18 {
+    public static void main(String[] args) {
+        String word = "DEMOCRACY";
+
+        //coding in reverse string to form the word "COME"
+        
+        StringBuffer sf =  new StringBuffer(word);
+        System.out.println(sf.reverse());
+        String rev = "YCARCOMED";
+        System.out.println(rev.substring(4,8));
+
+
+
+
+
+
+    }
+}
