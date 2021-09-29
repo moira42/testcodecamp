89b71e41896b30a76c63308708b42b27f5c38134
Moira Amadi-Emina <90391409+moira42@users.noreply.github.com>
2021-09-29 13:01:44 +0100

    Add files via upload

---
+++ ClassB.java
@@ -0,0 +1,14 @@
+package com.packageTwo;
+
+public class ClassB {
+    public static void main(String[] args) {
+        ClassB name = new ClassB();
+        name.classBMethod();
+
+    }
+
+
+    private void classBMethod() {
+        System.out.println("This is a method");
+    }
+}
