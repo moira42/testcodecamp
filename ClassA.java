c3e2a086b48ae36f025b46b089b9a026557f9512
Moira Amadi-Emina <90391409+moira42@users.noreply.github.com>
2021-09-29 12:56:28 +0100

    Add files via upload

---
+++ ClassA.java
@@ -0,0 +1,15 @@
+package com.packageOne;
+
+public class ClassA {
+    public static void main(String[] args) {
+        ClassA var = new ClassA();
+        var.classAMethod();
+
+    }
+
+
+    public void classAMethod(){
+        System.out.println("This is a method");
+    }
+
+}
