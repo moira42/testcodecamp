dc97c0abce7f1a6bf64c0f738684567460945a64
Moira Amadi-Emina <90391409+moira42@users.noreply.github.com>
2021-10-02 06:08:39 +0100

    Add files via upload

---
+++ Overloading.java
@@ -0,0 +1,26 @@
+package com.inheritance;
+
+public class Overloading {
+
+    //Overloading: Difference in number of data type
+
+    public void grains (String type){
+
+    }
+    public void grains (String type, String type2 ){
+    }
+      //Difference in the data type
+    public void grains (int number, int size){
+
+    }
+    public void grains (int size, boolean condition){
+
+    }
+       //Sequence of Data Types
+    public void grains (int number, float unit){
+
+    }
+    public void grains ( float unit, int number){
+
+    }
+}
