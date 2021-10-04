d1b1a617d9c950796f9f5fe316a9608087bc05fa
Moira Amadi-Emina <90391409+moira42@users.noreply.github.com>
2021-10-04 13:37:13 +0100

    Add files via upload

---
+++ ErrorHandling.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class ErrorHandling {
+    public static void main(String[] args) {
+
+        int age = Moira;
+        if (age != 12) {
+            throw new NoSuchFieldError("Cannot find variable");
+        }
+
+        System.out.println("no such field");
+
+
+    }
+}
+
+
+
+
